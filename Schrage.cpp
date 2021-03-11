43c2b6805d6d2807918c658b88cd8f0b6a9983b4
Slowikus <slowikstas@gmail.com>
2021-03-11 14:27:18 +0100

    poczatek tworzenia klasy schrage

---
+++ Schrage.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Slowikus on 11.03.2021.
+//
+
+#include "Schrage.h"
---
+++ Schrage.h
@@ -0,0 +1,20 @@
+//
+// Created by Slowikus on 11.03.2021.
+//
+#include <algorithm>
+#include <iostream>
+#include <list>
+
+
+#ifndef LAB2_SCHRAGE_H
+#define LAB2_SCHRAGE_H
+
+
+class Schrage {
+    std::list<int> r
+
+
+};
+
+
+#endif //LAB2_SCHRAGE_H
